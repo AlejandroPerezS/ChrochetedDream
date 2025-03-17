8abe8c13660e5c49db0c25f326701e8b72912c4c
Alejandro Perezgit config --global user.name Alejandro <alejandro.perezsanabria@dcmail.ca>
2025-03-16 21:25:24 -0400

    added some better layout

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,11 @@ function App() {
     return (
       <>
           <div className="container">
-              <div className="header">
-                  <div className="banner">
-                      <h1> 50% off your first order</h1>
-                  </div>
+              <div className="nav-bar">
                   <div className="logo">
                       <img src={siteLogo} alt="Crocheted Dream Logo"/>
                   </div>
-
+              </div>
                   <div className="menu">
                       <a className="log-in">
                           <h1 className="hide"> Please log in </h1>
@@ -28,13 +25,14 @@ function App() {
                       </label>
                   </div>
               </div>
-              <div className="content">
+              <div className="content"> Hello we are the Crocheted Dream! team and we would like to show you some of our products, we encourage you to contact us and dont be afraid nothing its too difficult for us
               </div>
+              <div className="gallery">adasd</div>
               <div className="footer">
 
               </div>
 
-          </div>
+
       </>
   )
 }
