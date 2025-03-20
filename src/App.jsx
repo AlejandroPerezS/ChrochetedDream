ec7910aefaf0dfc0c43d4905286ddf1afbfe8e97
Alejandro Perezgit config --global user.name Alejandro <alejandro.perezsanabria@dcmail.ca>
2025-03-20 08:05:45 -0400

    just playing with the layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,58 @@
-import siteLogo from './assets/logo.png'
-import './styles/App.css'
+import React from 'react';
+import siteLogo from './assets/logo.png';
+import './styles/App.css';
 
 function App() {
-    // noinspection JSValidateTypes
     return (
-        <>
+        <div className="page">
 
-                <div className="nav-bar">
-                    <div className="logo">
-                        <img src={siteLogo} alt="Crocheted Dream Logo" />
-                    </div>
-                    <div className="nav-links">
-                        <ul className="nav-link">
-                            <li><a href={siteLogo}>asdas</a></li>
-                            <li><a href={siteLogo}>asdas</a></li>
-                            <li><a href={siteLogo}>asdas</a></li>
-                            <li><a href={siteLogo}>asdas</a></li>
-                            <li><a href={siteLogo}>asdas</a></li>
-                        </ul>
-                    </div>
-                    <div className="logout">
-                        <a>Log out</a>
-                    </div>
+            <div className="nav-bar">
+                <div className="logo">
+                    <img src={siteLogo} alt="Crocheted Dream Logo" />
                 </div>
-
-            <div className="content-container">
-                <div className="content">
-                    Hello we are the Crocheted Dream! team and we would like to show you some of our products,
-                    we encourage you to contact us and dont be afraid nothing its too difficult for us
+                <div className="about-us">
+                    <p>Hello, we are the Crocheted Dream! team and we would like to show you some of our products.
+                        We encourage you to contact us and don’t be afraid nothing is too difficult for us.
+                    </p>
                 </div>
-                <div className="gallery">
-                    adasd
-                </div>
-                <div className="footer">
+                <div className="logout">
+                    <a href="#">Log out</a>
                 </div>
             </div>
+
+
+            <div className="content-container">
+                <div className="featured-items-grid">
+                    <div className="product-card">
+                        <img src={siteLogo} alt="Item Name"/>
+                        <h5>Item Name</h5>
+
+                        <p className="price">$XX.XX</p>
+                    </div>
+
+                </div>
+
+            </div>
+
+
             <div className="menu-container">
                 <div className="menu">
-                    <a className="log-in">
-                        <h1>Please log in</h1>
-                    </a>
-                    <a className="cart">
-                        <h1>Your Cart</h1>
-                    </a>
+                    <a className="log-in">Please log in</a>
+                    <a className="cart">Your Cart</a>
+                    <ul className="menu-links">
+                        <li><a href="#">Link 1</a></li>
+                        <li><a href="#">Link 2</a></li>
+                        <li><a href="#">Link 3</a></li>
+                        <li><a href="#">Link 4</a></li>
+                        <li><a href="#">Link 5</a></li>
+                    </ul>
                     <label>
-                        <input type="text" placeholder="Search.." className="search"/>
+                        <input type="text" placeholder="Search.." className="search" />
                     </label>
                 </div>
             </div>
-        </>
-    )
+        </div>
+    );
 }
 
-export default App
+export default App;
