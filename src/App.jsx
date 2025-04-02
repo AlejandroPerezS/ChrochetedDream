173416836ba645bc3626e1f88e9b395710e0a753
Alejandro <Alejandro.perezsanabria@dcmail.ca>
2025-04-02 10:58:53 -0400

    trying to add a form to submit custom orders,review and to get more iformation

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,35 @@ import siteLogo from './assets/logo.png';
 import './styles/App.css';
 
 function App() {
-    return (
+    function cutom_order() {
+
+    }
+
+    function order_item() {
+
+    }
+
+    function req_info() {
+
+    }
+
+    function review() {
+
+    }
+
+    let div = <>
         <div className="page">
 
             <div className="nav-bar">
                 <div className="logo">
-                    <img src={siteLogo} alt="Crocheted Dream Logo" />
+                    <img src={siteLogo} alt="Crocheted Dream Logo"/>
                 </div>
                 <div className="about-us">
                     <p>Hello, we are the Crocheted Dream! team and we would like to show you some of our products.
                         We encourage you to contact us and don’t be afraid nothing is too difficult for us.
                     </p>
                 </div>
-                <div className="logout">
-                    <a href="#">Log out</a>
-                </div>
+
             </div>
 
 
@@ -26,7 +40,6 @@ function App() {
                     <div className="product-card">
                         <img src={siteLogo} alt="Item Name"/>
                         <h5>Item Name</h5>
-
                         <p className="price">$XX.XX</p>
                     </div>
 
@@ -37,22 +50,26 @@ function App() {
 
             <div className="menu-container">
                 <div className="menu">
-                    <a className="log-in">Please log in</a>
-                    <a className="cart">Your Cart</a>
-                    <ul className="menu-links">
-                        <li><a href="#">Link 1</a></li>
-                        <li><a href="#">Link 2</a></li>
-                        <li><a href="#">Link 3</a></li>
-                        <li><a href="#">Link 4</a></li>
-                        <li><a href="#">Link 5</a></li>
-                    </ul>
-                    <label>
-                        <input type="text" placeholder="Search.." className="search" />
-                    </label>
+                    <h2> Fill the form for more information or to place an order</h2>
+
+                    <input type="text" placeholder="First Name"/>
+                    <br/>
+                    <select className="select" name="Select the most relevant option">
+                        <option value="order_custom_item" onSelect={cutom_order}>Make a custom order</option>
+                        <option value="order_item" onSelect={order_item}>Make order that is on the site</option>
+                        <option value="req_info" onSelect={req_info}>Request more information</option>
+                        <option value="review" onSelect={review}>Leave a review</option>
+                    </select>
+                    <br/>
+                    <label htmlFor="order_date">Please choose a date for your Order</label>
+                    <br/>
+                    <input type="date" id="order_date"/>
+
                 </div>
             </div>
         </div>
-    );
+    </>;
+    return div;
 }
 
 export default App;
